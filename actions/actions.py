14241731bbee2a52cd6e98faf2840ff1f756a21a
mukesh-nitp <mukesh.nitp30@gmail.com>
2021-10-02 04:51:27 +0530

    demo-code-commit

--- actions/actions.py
+++ actions/actions.py
@@ -1,33 +1,27 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
 
-from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.types import DomainDict
-from rasa_sdk.forms import FormAction
+# This is a simple example for a custom action which utters "Hello World!"
 
-class ActionPizzaOrderForm(FormAction):
-
-    def name(self) -> Text:
-        return "action_pizza_order_form"
-
-    @staticmethod
-    def required_slots(tracker: "Tracker") -> List[Text]:
-        return ["pizza_size","pizza_type","pizza_amount"]
-
-    def submit(
-        self,
-        dispatcher: "CollectingDispatcher",
-        tracker: "Tracker",
-        domain: "DomainDict",   
-    ) -> List[Dict]:
-
-        pizza_type = tracker.get_slot('pizza_type')
-        pizza_size = tracker.get_slot('pizza_size')
-        pizza_amount = tracker.get_slot('pizza_amount')
-
-        print(pizza_amount, pizza_type, pizza_size)
-
-        dispatcher.utter_message(text=f"ok Great. Your order is {pizza_amount} {pizza_type} pizza in {pizza_size} size. Can you please confirm your order?")
-
-        return []
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
